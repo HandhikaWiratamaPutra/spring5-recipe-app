f2aeb614bff8c6ef75726d1d94b5dddf91db9361
Handhika Wiratam Putra <yohaneshandhika98@gmail.com>
2023-01-08 12:32:23 +0700

    set Recipe to automaticly add Bi Directional data from class recipe to class Note and Ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -18,7 +18,13 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
-        public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
